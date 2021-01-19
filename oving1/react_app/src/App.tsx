8ad4d60add957bd68be007bb3bd40c1d91b85b73
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-01-19 19:58:08 +0100

    * Added footer and Gallery

--- oving1/react_app/src/App.tsx
+++ oving1/react_app/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import "./App.scss";
+import Gallery from "./components/Gallery/Gallery";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
